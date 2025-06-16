705414f4fa3df90e62cd3f5394ea582e46ba7f8c
haruki <kkhk0869hs33@gmail.com>
2025-06-16 17:08:19 +0000

    フロント、バックでコンテナを分けて開発
    注意点
    - 開発時はどちらか一方でしか開発できない。
    - 同時に開発できるように修正する予定

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,9 +1,11 @@
+import FastApi from "./FastApi";
 import Form from "./Form";
 import RaytracingCanvas from "./RaytracingCanvas";
 
 function Content() {
   return (
     <>
+      <FastApi />
       <Form />
       <RaytracingCanvas />
     </>
