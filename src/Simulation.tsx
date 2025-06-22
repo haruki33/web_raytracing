079f03cdfad79a9578f50d981a371341b75b57d4
haruki <kkhk0869hs33@gmail.com>
2025-06-22 12:34:55 +0000

    💄 レイアウト変更

--- src/Simulation.tsx
+++ src/Simulation.tsx
@@ -2,14 +2,19 @@ import Form from "./Form";
 import RaytracingCanvas from "./RaytracingCanvas";
 import type { ResponseData } from "./type";
 import { useState } from "react";
+import Container from '@mui/material/Container';
 
 function Simulation() {
   const [res, setRes] = useState<ResponseData | null>(null);
 
   return (
     <>
-      <Form setRes={setRes} />
-      <RaytracingCanvas res={res} />
+      <Container maxWidth="sm" sx={{ border: "1px solid #1a1a1a", padding: 2, marginTop: 2, borderRadius: 2 }}>
+        <Form setRes={setRes} />
+      </Container>
+      <Container maxWidth="sm" sx={{ border: "1px solid #1a1a1a", padding: 2, marginTop: 2, borderRadius: 2 }}>
+        <RaytracingCanvas res={res} />
+      </Container>
     </>
   );
 };
